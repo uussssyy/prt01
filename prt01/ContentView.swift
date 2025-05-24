348f81d63b2349d6da3bb52b55de5f9531c75f3f
uussssyy <ussy.1112@gmail.com>
2025-05-24 09:11:41 +0900

    Initial Commit

---
+++ prt01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prt01
+//
+//  Created by 牛田啓介 on 2025/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ prt01/prt01App.swift
@@ -0,0 +1,17 @@
+//
+//  prt01App.swift
+//  prt01
+//
+//  Created by 牛田啓介 on 2025/05/24.
+//
+
+import SwiftUI
+
+@main
+struct prt01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
