8e53437f2cb4b527fe762374847524a6fa83fcfe
uussssyy <ussy.1112@gmail.com>
2025-05-25 11:38:01 +0900

    test code

--- prt01/ContentView.swift
+++ prt01/ContentView.swift
@@ -11,6 +11,50 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+        Button(action: {
+            // ボタンがタップされた時の処理
+            print("Button tapped!")
+        }) {
+            Text("Tap me")
+        }
+        VStack {
+            Text("Item 1")
+            Text("Item 2")
+            Text("Item 3")
+        }
+        HStack {
+             Text("Left")
+             Spacer()
+             Text("Center")
+             Spacer()
+             Text("Right")
+         }
+        ZStack {
+            Rectangle()
+                .frame(width: 200, height: 200)
+                .foregroundColor(.blue)
+            
+            Text("Hello, ZStack!")
+                .foregroundColor(.white)
+        }
+        VStack {
+            HStack {
+                Text("Left")
+                Spacer()
+                Text("Center")
+                Spacer()
+                Text("Right")
+            }
+            
+            ZStack {
+                Rectangle()
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(.blue)
+                
+                Text("Hello, ZStack!")
+                    .foregroundColor(.white)
+            }
+        }
     }
 }
 
